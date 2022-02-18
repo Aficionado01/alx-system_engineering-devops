89e32f9073f6dd36074a41bd95b1118b95541e0f
Tonycrux <emmanuelugochukwu530@gmail.com>
2022-02-17 18:00:18 -0800

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,41 +1,18 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
+# Module that prints top 10 titles
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-
-
 def top_ten(subreddit):
-    '''Retrieves the title of the top ten posts from a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    sort = 'top'
-    limit = 10
-    res = requests.get(
-        '{}/r/{}/.json?sort={}&limit={}'.format(
-            BASE_URL,
-            subreddit,
-            sort,
-            limit
-        ),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        for post in res.json()['data']['children'][0:10]:
-            print(post['data']['title'])
-        else:
+    '''Prints titles of top 10 hot posts for subreddit'''
+    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    USERAGENT = {'User-Agent':
+                 'Unix:com.holberton.apiadvanced:task1 (by /u/_marc_marc_)'}
+    req = requests.get(URL, headers=USERAGENT)
+    if req.status_code is not 200:
         print(None)
+        return
+    jreq = req.json()
+    data_path = jreq['data']['children']
+    for i in range(10):
+        print(data_path[i]['data']['title'])
