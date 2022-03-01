7fd56b69411cfbdbecfb98a1ec2c753e6d3983d3
Tonycrux <emmanuelugochukwu530@gmail.com>
2022-03-01 04:20:26 -0800

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,95 +1,30 @@
 #!/usr/bin/python3
-"""Module fetches employe tasks and display on stdout."""
+'''A script that gathers data from an API.
+'''
+import re
+import requests
+import sys
 
 
-class Fetch:
-    """Class provides fetch services."""
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
-    def __init__(self, userId):
-        """Initialize fetch instance."""
 
-        payload = {"userId": userId}
-        userInfo = requests.get(
-            "https://jsonplaceholder.typicode.com/users/{}".format(
-                userId
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
             )
-        ).json()
-
-        self.userId = userId
-        self.name = userInfo.get("name")
-        self.userName = userInfo.get("username")
-        self.todos = requests.get(
-            "https://jsonplaceholder.typicode.com/todos/",
-            params=payload
-        ).json()
-
-        return None
-
-    def project_task_one(self):
-        """Returns information about employee TODO list progress
-        Args:
-            self (object): <class 'main.Fetch'> type object
-        Returns:
-            Comperhensive string of employee TODO list progress
-        """
-        brief, completed = "", ""
-        try:
-            brief = "Employee {} is done with tasks ({}/{}):\n".format(
-                self.name,
-                len(Fetch.__tasks(self.todos)["complete"]),
-                len(self.todos)
-            )
-            completed = Fetch.__completed_tasks(
-                Fetch.__tasks(self.todos)["complete"]
-            )
-        except Exception:
-            pass
-        return [brief, completed]
-
-    @staticmethod
-    def __tasks(tasks_list):
-        """returns dictionary of task lists acording to completeness.
-        Args:
-            tasks_list (list): List of tasks fetched.
-            status (str): `finished` or `unfinished`.
-        Returns:
-            dictionary of completed and uncompleted task lists.
-        """
-        status_dict = {
-            "complete": [],
-            "incomplete": []
-        }
-
-        for elem in tasks_list:
-            if elem.get("completed") is True:
-                status_dict["complete"].append(elem)
-        for elem in tasks_list:
-            if elem not in status_dict["complete"]:
-                status_dict["incomplete"].append(elem)
-        return status_dict
-
-    @staticmethod
-    def __completed_tasks(tasks_list):
-        """Returns string representation of all tasks in provided list
-        Args:
-            tasks_list (list): List of tasks fetched.
-        Returns:
-            Tabulated list of completed tasks
-        """
-        brief = ""
-        for elem in tasks_list:
-            brief += "\t {}\n".format(elem.get("title"))
-        return brief
-    pass
-
-
-if __name__ == "__main__":
-    import requests
-    import sys
-
-    try:
-        fetch = Fetch(sys.argv[1])
-        for elem in fetch.project_task_one():
-            print(elem, end="")
-    except Exception:
-        pass
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
